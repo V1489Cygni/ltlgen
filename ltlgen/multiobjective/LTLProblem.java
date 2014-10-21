e96a62f9e3acd62df9b267c4763bc3adb6afaa7e
V1489Cygni <wb0011@mail.ru>
2014-10-22 03:43:15 +0400

    Mutants used

--- ltlgen/multiobjective/LTLProblem.java
+++ ltlgen/multiobjective/LTLProblem.java
@@ -14,6 +14,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class LTLProblem extends GPProblem {
@@ -21,6 +23,10 @@ public class LTLProblem extends GPProblem {
     public static int ACTION_NUMBER;
     private static FST automaton;
     private static FST[] testAutomatons;
+    private static FST[] mutants;
+    private static Map<String, Double> correctness = new HashMap<>();
+    private static Map<String, Double> results = new HashMap<>();
+    private static Map<String, Double> mResults = new HashMap<>();
 
     @Override
     public void setup(final EvolutionState state, final Parameter base) {
@@ -30,16 +36,16 @@ public class LTLProblem extends GPProblem {
         int stateNumber = state.parameters.getInt(base.push("automaton").push("state-number"), null);
         automaton = Verifier.loadFST(state.parameters.getString(base.push("automaton").push("file"), null), stateNumber);
         testAutomatons = new FST[state.parameters.getInt(base.push("test-automatons").push("number"), null)];
-        int mutatedNumber = state.parameters.getInt(base.push("test-automatons").push("mutated"), null);
-        if (mutatedNumber > testAutomatons.length) {
-            state.output.fatal("Number of mutated automatons should not be greater than total number of automatons");
-        }
+        mutants = new FST[state.parameters.getInt(base.push("test-automatons").push("mutated"), null)];
         RandomProvider.initialize(1, System.currentTimeMillis());
         RandomProvider.register();
-        for (int i = 0; i < mutatedNumber; i++) {
-            testAutomatons[i] = automaton.mutate(false);
+        for (int i = 0; i < mutants.length; i++) {
+            mutants[i] = automaton.mutate(false);
         }
-        for (int i = mutatedNumber; i < testAutomatons.length; i++) {
+        /*for (int i = mutants.length / 2; i < mutants.length; i++) {
+            mutants[i] = mutants[i].mutate(false);
+        }*/
+        for (int i = 0; i < testAutomatons.length; i++) {
             try {
                 Process p = Runtime.getRuntime().exec("./generate-efsm -n " + stateNumber + " -e " + EVENT_NUMBER + " -a " + ACTION_NUMBER + " -m 0 -s 2 -v 2");
                 Scanner sc = new Scanner(p.getInputStream());
@@ -65,17 +71,33 @@ public class LTLProblem extends GPProblem {
             ((GPIndividual) ind).trees[0].child.eval(state, threadnum, input, stack, ((GPIndividual) ind), this);
             SPEA2MultiObjectiveFitness f = (SPEA2MultiObjectiveFitness) ind.fitness;
             String formula = "G(" + input.result + ")";
-            double result = 0, size = 0;
-            double r = Verifier.getVerifiedTransitionsRatio(automaton, formula);
+            double result = 0, size = 0, mRes = 0, r;
+            if (correctness.containsKey(formula)) {
+                r = correctness.get(formula);
+                result = results.get(formula);
+                mRes = mResults.get(formula);
+            } else {
+                r = Verifier.getVerifiedTransitionsRatio(automaton, formula);
+                if (r >= 0.75) {
+                    for (FST fst : testAutomatons) {
+                        double t = Verifier.getVerifiedTransitionsRatio(fst, formula);
+                        result += Math.pow(t, 2);
+                    }
+                    result = 1 - Math.sqrt(result) / testAutomatons.length;
+                    for (FST fst : mutants) {
+                        double t = Verifier.getVerifiedTransitionsRatio(fst, formula);
+                        mRes += Math.pow(t, 2);
+                    }
+                    mRes = 1 - Math.sqrt(mRes) / mutants.length;
+                }
+                correctness.put(formula, r);
+                results.put(formula, result);
+                mResults.put(formula, mRes);
+            }
             if (r >= 0.75) {
                 size = 1 / (1.0 + input.size);
-                for (FST fst : testAutomatons) {
-                    double t = Verifier.getVerifiedTransitionsRatio(fst, formula);
-                    result += Math.pow(t, 2);
-                }
-                result = 1 - Math.sqrt(result) / testAutomatons.length;
             }
-            f.setObjectives(state, new double[]{r, result, size});
+            f.setObjectives(state, new double[]{r, result, mRes, size});
             ind.evaluated = true;
         }
     }
